aad6d69eab2d46de2edcb80744ad94f59f54c5c0
jamyzswitch <jamyzswitch@gmail.com>
2024-07-19 12:16:18 +0200

    Use RPi.GPIO

--- SafeShutdown.py
+++ SafeShutdown.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-from gpiozero import Button, LED
+import RPi.GPIO as GPIO
 import os
-from signal import pause
 import time
 
 powerPin = 3
@@ -9,27 +8,44 @@ resetPin = 2
 ledPin = 14
 powerenPin = 4
 hold = 1
-led = LED(ledPin)
-led.on()
-power = LED(powerenPin)
-power.on()
 
-def when_pressed():
-    led.blink(.2, .2)
+# Configuration de la bibliothèque RPi.GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(powerPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(resetPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(powerenPin, GPIO.OUT)
+
+# Allumer les LEDs initialement
+GPIO.output(ledPin, GPIO.HIGH)
+GPIO.output(powerenPin, GPIO.HIGH)
+
+def when_pressed(channel):
+    GPIO.output(ledPin, GPIO.LOW)
+    time.sleep(0.2)
+    GPIO.output(ledPin, GPIO.HIGH)
+    time.sleep(0.2)
+    GPIO.output(ledPin, GPIO.LOW)
+    time.sleep(0.2)
+    GPIO.output(ledPin, GPIO.HIGH)
+    time.sleep(0.2)
     time.sleep(2)
     os.system("sudo shutdown -h now")
 
-def when_released():
-    led.on()
+def when_released(channel):
+    GPIO.output(ledPin, GPIO.HIGH)
 
-def reboot():
-    #select hard or safe reboot
-    os.system("sudo reboot")#comment or uncomment for hard reboot
-    #os.system("sudo shutdown -r now")# comment or uncomment for safe reboot
+def reboot(channel):
+    # Sélectionner redémarrage hard ou safe
+    os.system("sudo reboot")  # décommenter pour redémarrage hard
+    # os.system("sudo shutdown -r now")  # décommenter pour redémarrage safe
 
-btn = Button(powerPin, hold_time=hold)
-rebootBtn = Button(resetPin)
-rebootBtn.when_pressed = reboot
-btn.when_pressed = when_pressed
-btn.when_released = when_released
-pause()
+# Définir les événements
+GPIO.add_event_detect(powerPin, GPIO.FALLING, callback=when_pressed, bouncetime=200)
+GPIO.add_event_detect(resetPin, GPIO.FALLING, callback=reboot, bouncetime=200)
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
